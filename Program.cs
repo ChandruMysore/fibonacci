538fa81fbc39a1b53b025523867461ac863bd81a
Chandrashekar <chandrashekhara.kv@artefactsys.com>
2021-04-24 14:52:08 +0530

    Changes related to Fibonacci series generation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        public static void Fibonacci(int x)
+        {
+            var previousValue = -1;
+            var currentResult = 1;
+
+            for (var i = 0; i < x; ++i)
+            {
+                var sum = currentResult + previousValue;
+                previousValue = currentResult;
+                currentResult = sum;
+                Console.Write("{0} ", currentResult);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(" Input number for the Fibonacci series : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n The Fibonacci series of {0} terms is : ",n1);
+	        Fibonacci(n1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# fibonacci
+# Fibonacci Series Generation 
+
+This project was generated with .net framework 5.0 using asp.net core console application
+
+The Program file contains a logic to generate the first n Fibonacci Numbers.
+
+## Description
+1. Console ask for the User Input to generate the series.
+2. Based on the user input 'N' the series will generate.
+3. Implemented using flat line logic to maintain the performence measure.
+
+## Installation
+dotnet run
+
